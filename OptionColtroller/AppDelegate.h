a897d68ed62d96b5a96e87afc42e8616f2edff62
zm <jiannan@6rooms.com>
2015-07-01 15:36:17 +0800

    Initial Commit

---
+++ OptionColtroller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OptionColtroller
+//
+//  Created by zm on 15/7/1.
+//  Copyright (c) 2015年 zm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
